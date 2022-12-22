30eb19cb2735951045de2dab48434fc29c7baa8e
Memo <77158623+Selle007@users.noreply.github.com>
2022-12-22 01:21:07 +0100

    Added Migrations

---
+++ AniWatch/AniWatch/Data/Migrations/20221222001622_Created_anime_and_categories.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AniWatch.Data.Migrations
+{
+    public partial class Created_anime_and_categories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animes",
+                columns: table => new
+                {
+                    AnimeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AnimeName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AnimeDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AnimeStudio = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animes", x => x.AnimeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryDesc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimeCategories",
+                columns: table => new
+                {
+                    AnimeCategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: true),
+                    AnimeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimeCategories", x => x.AnimeCategoryId);
+                    table.ForeignKey(
+                        name: "FK_AnimeCategories_Animes_AnimeId",
+                        column: x => x.AnimeId,
+                        principalTable: "Animes",
+                        principalColumn: "AnimeId");
+                    table.ForeignKey(
+                        name: "FK_AnimeCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimeCategories_AnimeId",
+                table: "AnimeCategories",
+                column: "AnimeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimeCategories_CategoryId",
+                table: "AnimeCategories",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimeCategories");
+
+            migrationBuilder.DropTable(
+                name: "Animes");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
